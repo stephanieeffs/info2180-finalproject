e01b4665570e6fa6c7b93fe40aa445b56c752496
KMcMillan7 <kamalthegeneral@hotmail.com>
2024-12-14 17:27:08 -0500

    update viewcontact.php

---
+++ viewcontact.php
@@ -0,0 +1,47 @@
+<?php
+
+session_start();
+if(!isset($_SESSION['id'])){
+    session_destroy();
+    header('Location: login.php');
+    exit;
+}
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$db_name = "dolphin_crm";
+
+$conn = new PDO("mysql:host=$host; dbname=$db_name; charset=utf8mb4",$username, $password);
+
+$id = filter_var($_GET['view'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+//GETTING THE CONTACT
+$stmt = $conn->prepare("SELECT * FROM contacts where id=$id");
+$stmt->execute();
+$row = $stmt->fetchAll(PDO::FETCH_ASSOC)[0];
+
+//GETTING THE USER THAT CREATED THE CONTACT
+$stmt = $conn->prepare("SELECT * FROM users where id= " . $row['created_by']);
+$stmt->execute();
+$created_by= $stmt->fetchAll(PDO::FETCH_ASSOC)[0];
+
+//GETTING THE USER THAT CONTACT IS ASSIGNED TO
+$stmt = $conn->prepare("SELECT * FROM users where id= " . $row['assigned_to']);
+$stmt->execute();
+$assigned_to= $stmt->fetchAll(PDO::FETCH_ASSOC)[0];
+
+
+function convertDateFormat($date){
+    $date = explode("-", $date);
+    $monthNum  = $date[1];
+    $dateObj   = DateTime::createFromFormat('!m', $monthNum);
+    $monthName = $dateObj->format('F');
+    return $monthName . " " . $date[2] . " " . $date[0];
+}
+
+function convertTimeFormat(){
+
+}
+
+?>
