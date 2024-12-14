5a4b33619c29d73be0eb223f5b739a1c24ecd024
kylepeart21 <kylepeart17@gmail.com>
2024-12-13 21:07:56 -0500

    Changed the sql

--- schema.sql
+++ schema.sql
@@ -1,125 +1,49 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 06, 2024 at 05:57 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+DROP DATABASE IF EXISTS dolphin_crm;
+CREATE DATABASE dolphin_crm;
 USE dolphin_crm;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+    `id` int(11) NOT NULL auto_increment,
+    `firstname` VARCHAR(250) NOT NULL,
+    `lastname` VARCHAR(250) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(50) NOT NULL,
+    `role` ENUM('Admin', 'User') NOT NULL,
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (`id`)
+);
 
---
--- Database: `dolphin_crm`
---
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `password`, `email`, `role`, `created_at`) VALUES
+(2, 'Admin', 'Project', '$2y$10$RpUYkS5zi/Z/caRn9F7n.eCT1EtQD7C2Szpm8Mun.tR8J3aCkg4dG', 'admin@project2.com', 'Admin', '2022-12-07 06:40:17');
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `contacts`;
+CREATE TABLE `contacts` (
+    `id` int(11) NOT NULL auto_increment,
+    `title` VARCHAR(50) NOT NULL,
+    `firstname` VARCHAR(250) NOT NULL,
+    `lastname` VARCHAR(250) NOT NULL,
+    `email` VARCHAR(50) NOT NULL,
+    `telephone` VARCHAR(15) NOT NULL,
+    `company` VARCHAR(250) NOT NULL,
+    `type` VARCHAR(15) NOT NULL,
+    `assigned_to` int(11) NOT NULL,
+    `created_by` int(11) NOT NULL,
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (`id`),
+FOREIGN KEY (`assigned_to`) REFERENCES `users`(`id`),
+FOREIGN KEY (`created_by`) REFERENCES `users`(`id`)
+);
 
---
--- Table structure for table `contacts`
---
-
-CREATE TABLE IF NOT EXISTS `contacts` (
-  `id` int(11) NOT NULL,
-  `title` varchar(255) DEFAULT NULL,
-  `firstname` varchar(255) DEFAULT NULL,
-  `lastname` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `telephone` varchar(50) DEFAULT NULL,
-  `company` varchar(255) DEFAULT NULL,
-  `type` varchar(50) DEFAULT NULL,
-  `assigned_to` int(11) DEFAULT NULL,
-  `created_by` int(11) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `notes`
---
-
-CREATE TABLE IF NOT EXISTS `notes` (
-  `id` int(11) NOT NULL,
-  `contact_id` int(11) DEFAULT NULL,
-  `comment` text DEFAULT NULL,
-  `created_by` int(11) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL,
-  `firstname` varchar(128) DEFAULT NULL,
-  `lastname` varchar(128) DEFAULT NULL,
-  `password` varchar(128) DEFAULT NULL,
-  `email` varchar(128) DEFAULT NULL,
-  `role` varchar(100) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-INSERT INTO users (password, email)
-VALUES (SHA2('password123', 256),'admin@project2.com' );
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `contacts`
---
-ALTER TABLE `contacts`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `notes`
---
-ALTER TABLE `notes`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `contacts`
---
-ALTER TABLE `contacts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `notes`
---
-ALTER TABLE `notes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+DROP TABLE IF EXISTS `notes`;
+CREATE TABLE `notes`(
+    `id` int(11) NOT NULL auto_increment,
+    `contact_id` int(11) NOT NULL,
+    `comment` TEXT NOT NULL,
+    `created_by` int(11) NOT NULL,
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (`id`),
+FOREIGN KEY (`contact_id`) REFERENCES `contacts`(`id`),
+FOREIGN KEY (`created_by`) REFERENCES `users`(`id`)
+);
